fa7685087aef0466f2f9a8a4c1e66406f8897725
Ilya8741 <ilyha874@mail.ru>
2026-02-05 22:15:36 +0300

    p fix v2

--- template-parts/sections/policy-page.php
+++ template-parts/sections/policy-page.php
@@ -1,7 +1,14 @@
 <div class="policy-page">
     <div class="policy-page-wrapper">
         <h1 class="policy-page-title" data-aos="fade-right" data-aos-duration="600" data-aos-delay="100" data-aos-easing="ease-out"><?php the_sub_field('title'); ?></h1>
-                <div class="policy-page-subtitle" data-aos="fade-left" data-aos-duration="600" data-aos-delay="100" data-aos-easing="ease-out"><?php the_sub_field('subtitle'); ?></div>
+                <div class="policy-page-subtitle" data-aos="fade-left" data-aos-duration="600" data-aos-delay="100" data-aos-easing="ease-out">
+                     <?php
+                $subtitle = get_sub_field('subtitle');
+                if ($subtitle) {
+                    echo apply_filters('the_content', $subtitle);
+                }
+                ?>
+                </div>
                    <?php if ( have_rows('content_repeater') ) : ?>
                     <div class="policy-page-content-wrapper" data-aos="fade-right" data-aos-duration="600" data-aos-delay="100" data-aos-easing="ease-out">
       <?php while ( have_rows('content_repeater') ) : the_row(); ?>
